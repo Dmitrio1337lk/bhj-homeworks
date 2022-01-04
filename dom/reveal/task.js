3f117972ab08046afc42832108985f621872966f
Dmitrio <ar-dimasik@ya.ru>
2022-01-04 10:51:48 +0100

    Homework 4

---
+++ dom/reveal/task.js
@@ -0,0 +1,23 @@
+const banners = document.querySelectorAll('.reveal');
+
+const viewPortSize = window.innerHeight;
+
+window.addEventListener('scroll', function() {
+
+    for (let item of banners) {
+
+        const ads = {tob, bottom} = item.getBoundingClientRect();
+
+        if ((viewPortSize - ads.top - 120 >= 0) && (ads.bottom - 100 >= 0)) { 
+
+            item.classList.add('reveal_active');
+
+        }
+
+        else {
+
+            item.classList.remove('reveal_active');
+
+        }
+    }
+});
